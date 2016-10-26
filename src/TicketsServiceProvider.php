0be38496617650d6de5a5cf96898ba80b2f47150
Branden <branden@pureintellect.com>
2016-10-25 23:15:14 -0400

    Working on migrations

--- src/TicketsServiceProvider.php
+++ src/TicketsServiceProvider.php
@@ -11,11 +11,11 @@ class TicketsServiceProvider extends ServiceProvider
 	 * @return void
 	 */
 	 public function boot(){
-	 	require __DIR__ . '/../../../../vendor/autoload.php';
+	 	require __DIR__ . '/../../vendor/autoload.php';
 	 	$this->loadViewsFrom(__DIR__.'/resources/views', 'tickets');
-	 	$this->loadMigrationsFrom(__DIR__.'/resources/migrations');
+	 	//	$this->loadMigrationsFrom(__DIR__.'/resources/migrations', 'tickets');
 
-		$this->publishes([__DIR__.'/resources/migrations/' => database_path('/migrations')], 'migrations');
+		$this->publishes([__DIR__.'/resources/migrations/' => database_path('migrations')], 'migrations');
 
 	 }
 
@@ -24,8 +24,8 @@ class TicketsServiceProvider extends ServiceProvider
 	  *
 	  * @return void
 	  */
-	  public function register(){
+	public function register(){
 		include __DIR__.'/routes.php';
 		$this->app->make('PureIntellect\Tickets\Controllers\TicketController');
-	  }
+  }
 }
