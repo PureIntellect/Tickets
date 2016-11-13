bd7ce16e0111ee568dca5f9e30afb3354b446e29
Branden <branden@pureintellect.com>
2016-11-13 15:32:55 -0500

    Playing with routes

--- src/routes.php
+++ src/routes.php
@@ -2,21 +2,21 @@
 
 /* Public Routes */
 
-Route::group(['prefix' => 'tickets', 'middleware'=>'web'], function($router){
+Route::group(['prefix' => 'pi/tickets', 'middleware'=>'web'], function($router){
 	$router->get('/', 'PureIntellect\Tickets\Controllers\TicketController@index');
 
-	$router->get('create', 'TicketController@create');
-	$router->post('create', 'TicketController@store');
+	$router->get('/create', 'PureIntellect\Tickets\Controllers\TicketController@create');
+	$router->post('/create', 'PureIntellect\Tickets\Controllers\TicketController@store');
 
-	$router->get('/{ticket_id}', 'TicketController@show');
-	$router->get('/{ticket_id}/edit', 'TicketController@edit');
-	$router->post('/{ticket_id}/edit', 'TicketController@update');
+	$router->get('/{ticket_id}', 'PureIntellect\Tickets\Controllers\TicketController@show');
+	$router->get('/{ticket_id}/edit', 'PureIntellect\Tickets\Controllers\TicketController@edit');
+	$router->post('/{ticket_id}/edit', 'PureIntellect\Tickets\Controllers\TicketController@update');
 
-	$router->post('comment', 'TicketCommentsController@postComment');
+	$router->post('/comment', 'PureIntellect\Tickets\Controllers\TicketCommentsController@postComment');
 });
 
 /* Staff Routes */
-Route::group(['prefix' => 'admin', 'middleware'=>['web','dev']], function($router) {
-	$router->get('tickets', 'PureIntellect\Tickets\Controllers\TicketController@index');
-	$router->post('close_ticket/{ticket_id}', 'PureIntellect\Tickets\Controllers\TicketController@close');
+Route::group(['prefix' => 'pi/tickets', 'middleware'=>['web','dev']], function($router) {
+	$router->get('/', 'PureIntellect\Tickets\Controllers\TicketController@index');
+	$router->post('/close_ticket/{ticket_id}', 'PureIntellect\Tickets\Controllers\TicketController@close');
 });
