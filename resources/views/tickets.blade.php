0803a6f88031ac8d7d2ddef882a5e9ef24a431fe
Branden <branden@pureintellect.com>
2016-12-24 09:16:03 -0500

    Fixed child element for vue2

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -1,20 +1,19 @@
 <spark-kiosk-tickets inline-template>
-  <!-- Create Ticket -->
-  <div class="panel panel-default">
+  <div>
+    <!-- Create Ticket -->
+    <div class="panel panel-default">
       <div class="panel-heading">Create Ticket</div>
-
       <div class="panel-body">
           <div class="alert alert-info">
               Create a ticket for a customer
           </div>
-
           <form class="form-horizontal" role="form">
             <div class="row">
               <div class="col-md-4">
+
                 <!-- User -->
                 <div class="form-group">
                     <label class="col-md-4 control-label">User</label>
-
                     <div class="col-md-6">
                         <select class="form-control" name="user_id" v-model="newTicket.user_id">
                             <option value="">Choose User...</option>
@@ -22,6 +21,7 @@
                         </select>
                     </div>
                 </div>
+
                 <!-- Category -->
                 <div class="form-group">
                     <label class="col-md-4 control-label">Category</label>
@@ -32,6 +32,7 @@
                         </select>
                     </div>
                 </div>
+
                 <!-- Status -->
                 <div class="form-group">
                     <label class="col-md-4 control-label">Status</label>
@@ -42,6 +43,7 @@
                         </select>
                     </div>
                 </div>
+
                 <!-- Priority -->
                 <div class="form-group">
                     <label class="col-md-4 control-label">Priority</label>
@@ -53,7 +55,9 @@
                     </div>
                 </div>
               </div>
+
               <div class="col-md-8">
+
                 <!-- Ticket -->
                 <div class="form-group">
                     <label class="col-md-2 control-label">Ticket</label>
@@ -62,6 +66,7 @@
                         </textarea>
                     </div>
                 </div>
+
                 <!-- Create Button -->
                 <div class="form-group">
                     <div class="col-md-offset-4 col-md-6" class="pull-right">
@@ -74,55 +79,55 @@
           </form>
       </div>
   </div>
-    <!-- Open Tickets List -->
-        <div class="panel panel-default" v-cloak>
-            <div class="panel-heading">Recent Tickets</div>
-            <div class="panel-body">
-                <table class="table table-borderless m-b-none">
-                    <thead>
-                        <th>Created By</th>
-                        <th>Date</th>
-                        <th>User</th>
-                        <th>Category</th>
-                        <th></th>
-                    </thead>
+  <!-- Open Tickets List -->
+  <div class="panel panel-default" v-cloak>
+    <div class="panel-heading">Recent Tickets</div>
+      <div class="panel-body">
+        <table class="table table-borderless m-b-none">
+          <thead>
+            <th>Created By</th>
+            <th>Date</th>
+            <th>User</th>
+            <th>Category</th>
+            <th></th>
+          </thead>
+          <tbody>
+            <tr v-for="ticket in tickets">
+              <!-- Photo -->
+              <td>
+                <img v-if="ticket.creator" :src="ticket.creator.photo_url" class="spark-profile-photo">
+              </td>
 
-                    <tbody>
-                        <tr v-for="ticket in tickets">
-                            <!-- Photo -->
-                            <td>
-                                <img v-if="ticket.creator" :src="ticket.creator.photo_url" class="spark-profile-photo">
-                            </td>
+              <!-- Date -->
+              <td>
+                <div class="btn-table-align">
+                  @{{ ticket.created_at | datetime }}
+                </div>
+              </td>
+              <td>
+                <img :src="ticket.user.photo_url" class="spark-profile-photo">
+              </td>
+              <td>
+                @{{ticket.category}}
+              </td>
 
-                            <!-- Date -->
-                            <td>
-                                <div class="btn-table-align">
-                                    @{{ ticket.created_at | datetime }}
-                                </div>
-                            </td>
-                            <td>
-                                <img :src="ticket.user.photo_url" class="spark-profile-photo">
-                            </td>
-                            <td>
-                                @{{ticket.category}}
-                            </td>
+              <!-- Edit Button -->
+              {{-- <td>
+                <button class="btn btn-primary" @click="editAnnouncement(announcement)">
+                  <i class="fa fa-pencil"></i>
+                </button>
+              </td> --}}
 
-                            <!-- Edit Button -->
-                            {{-- <td>
-                                <button class="btn btn-primary" @click="editAnnouncement(announcement)">
-                                    <i class="fa fa-pencil"></i>
-                                </button>
-                            </td> --}}
-
-                            <!-- Delete Button -->
-                            <td>
-                                {{-- <button class="btn btn-danger-outline" @click="approveAnnouncementDelete(announcement)">
-                                    <i class="fa fa-times"></i>
-                                </button> --}}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+              <!-- Delete Button -->
+              <td>
+                {{-- <button class="btn btn-danger-outline" @click="approveAnnouncementDelete(announcement)">
+                  <i class="fa fa-times"></i>
+                </button> --}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </spark-kiosk-notify>
