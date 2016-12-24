19bdc84864eaf16e094bb0e1dc300327338d29c3
Branden <branden@pureintellect.com>
2016-12-24 10:45:16 -0500

    working on controller

--- src/Controllers/TicketController.php
+++ src/Controllers/TicketController.php
@@ -15,31 +15,32 @@ class TicketController extends Controller
 	public function create()
 	{
 		$categories = TicketCategory::all();
+		$priorities = TicketPriority::all();
+		$statuses = TicketStatus::all();
 		return view('tickets::create', compact('categories'));
 	}
 
-	public function store(Request $request, AppMailer $mailer)
+	public function store(Request $request)
 	{
     	$this->validate($request, [
-				'title'     => 'required',
+				'user_id'     => 'required|email',
 				'category'  => 'required',
 				'priority'  => 'required',
 				'message'   => 'required'
       ]);
 
       $ticket = new Ticket([
-				'title' 				=> $request->input('title'),
-				'user_email'	 	=> Auth::user()->email,
+				'user_email'	 	=> $request->input('user_id'),
         'ticket_id' 		=> strtoupper(str_random(10)),
         'category_id'  	=> $request->input('category'),
       	'priority'  		=> $request->input('priority'),
         'message'   		=> $request->input('message'),
-      	'status'    		=> "Open",
+      	'status'    		=> 1,
       ]);
 
       $ticket->save();
 			//Mail::to(Auth::user()->email)->send(new TicketStatus);
-      $mailer->sendTicketInformation(Auth::user(), $ticket);
+      //$mailer->sendTicketInformation(Auth::user(), $ticket);
 
       return redirect()->back()->with("status", "A ticket with ID: #$ticket->ticket_id has been opened.");
 	}
