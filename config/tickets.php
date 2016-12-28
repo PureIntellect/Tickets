e36f4af97a1c74c203dd6c0e72d1071f074cafb0
Branden <branden@pureintellect.com>
2016-12-28 16:21:28 -0500

    Update config: 	Added options for placeholder/reminder of future features

--- config/tickets.php
+++ config/tickets.php
@@ -1,8 +1,21 @@
 <?php
 
 return [
-  'sendFrom' => 'support@pureintellect.com',
+  // When emails are sent who are they from
+  send => [
+      'from' => 'PureIntellect.Com Support Team',
+      'signature' => '',
+  ],
 
+
+  // Active or Registered users ability to create Tickets
+  // Unregistered users will be stored in user_email, all others will be stored in user via the user id
+  'active_only' => false,
+
+  'comments' => true,
+
+  //Statistics and Reports
+  'statistics'  => false,
 ]
 
 ?>
